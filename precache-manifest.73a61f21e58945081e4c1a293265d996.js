553d6299120fd338bb928ecd0f0d166f0311448d
Dima Baranov <vajnoe-dima@mail.ru>
2018-12-03 12:00:24 +0300

    Updates

---
+++ precache-manifest.73a61f21e58945081e4c1a293265d996.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d6a1fcc8174caf5a5bc",
+    "url": "/tele-school/static/js/runtime~main.2d6a1fcc.js"
+  },
+  {
+    "revision": "87effd9b4264bed97482",
+    "url": "/tele-school/static/js/main.87effd9b.chunk.js"
+  },
+  {
+    "revision": "ee333d3b9d90a9438031",
+    "url": "/tele-school/static/js/1.ee333d3b.chunk.js"
+  },
+  {
+    "revision": "87effd9b4264bed97482",
+    "url": "/tele-school/static/css/main.7c577a11.chunk.css"
+  },
+  {
+    "revision": "ee333d3b9d90a9438031",
+    "url": "/tele-school/static/css/1.8fc9ba54.chunk.css"
+  },
+  {
+    "revision": "76191684b3e94f3a3f170f52d2971a72",
+    "url": "/tele-school/index.html"
+  }
+];
